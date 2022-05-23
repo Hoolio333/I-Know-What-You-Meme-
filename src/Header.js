987db9e5401a31f99b3d587e11e54b133264aa21
Chris Hyde <c_hyde333@hotmail.com>
2022-05-23 17:43:49 +0100

    Progress so far

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img
+        src="https://i.gyazo.com/e6ffb2f3a73fca6cf7ee17edb6e37692.png"
+        className="header--image"
+      />
+      <h2 className="header--title">I Know What You Meme!</h2>
+      <span className="emoji">&#128514;</span>
+      <span className="emoji">&#128514;</span>
+      <span className="emoji">&#128514;</span>
+      {/* <h4 className="header--project">Meme Generator</h4> */}
+    </header>
+  );
+}
+
+<span style="font-size:100px;">&#128514;</span>;
